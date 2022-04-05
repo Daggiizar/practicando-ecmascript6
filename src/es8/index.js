4d1a7fde99a70bdee9fc574a69022d31cc53ff4a
Daniel Zárate <d.ansizgz@gmail.com>
2022-04-05 15:26:35 -0500

    caso Async Away es8

--- src/es8/index.js
+++ src/es8/index.js
@@ -1,3 +1,4 @@
+//----------------------------Object.entries----------------------
 const data = {
     frontend: 'Daniel',
     backend: 'Isabel',
@@ -8,6 +9,9 @@ const entries = Object.entries(data);
 console.log(entries);
 console.log(entries.length);
 
+//----------------------------Object.entries----------------------
+
+//----------------------------Object.values----------------------
 const data= {
     frontend: 'Daniel',
     backend: 'Isabel',
@@ -18,9 +22,42 @@ const values = Object.values(data);
 console.log(values);
 console.log(values.length);
 
+//----------------------------Object.values----------------------
 
+//----------------------------padStart & padEnd----------------------
 const string = 'hello';
 console.log(string.padStart(7,'hi'));
 console.log(string.padEnd(12, ' -----'));
 console.log('food'.padEnd(12, '  -----'));
 
+//----------------------------padStart & padEnd----------------------
+
+//----------------------------Async Await----------------------
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) =>{
+        (true)
+            ? setTimeout (() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
+
+//----------------------------Async Await----------------------
